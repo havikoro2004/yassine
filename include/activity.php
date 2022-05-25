5aa17fdcf8204f7d82e1a0fc7191879814393e59
flata <havikoro2004@gmail.com>
2022-05-25 19:14:35 +0200

    Finir Activity

--- include/activity.php
+++ include/activity.php
@@ -2,11 +2,13 @@
 $db = new PDO ('mysql:host=localhost;dbname=club','root','');
 $alert =null;
             if (isset($_POST['addBtn'])){$db = new PDO ('mysql:host=localhost;dbname=club','root','');
-                $reqActivity = $db ->prepare('select * from activity order by name ');
+                $reqActivity = $db ->prepare('select * from activity where name=:name ');
+                $reqActivity->bindParam(':name',$_POST['add']);
                 $reqActivity->execute();
                 if (!$reqActivity->fetch()){
                     $reqAdd = $db->prepare('insert into activity (name) values (:name) ');
-                    $reqAdd->bindParam(':name',$_POST['add']);
+                    $addValue = strtoupper($_POST['add']);
+                    $reqAdd->bindParam(':name',$addValue);
                     $reqAdd->execute();
                     $alert = '<div class="alert alert-success mt-3 container text-center" role="alert"><h4>L\'activité a bien été ajoutée</h4></div>';
                     header( "refresh:1;url=activity.php" );
