4d275c4d1c4f68275cc3a790feb6e2720945ceb0
flata <havikoro2004@gmail.com>
2022-06-17 17:49:20 +0200

    Save db tables edits

--- databaseSave/backup.sql
+++ databaseSave/backup.sql
@@ -1,34 +1,29 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.6.5-MariaDB, for Win64 (AMD64)
 --
--- Hôte : localhost
--- Généré le : jeu. 16 juin 2022 à 13:42
--- Version du serveur : 10.3.31-MariaDB-0+deb10u1
--- Version de PHP : 7.3.31-1~deb10u1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: club
+-- ------------------------------------------------------
+-- Server version	10.6.5-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données : `debut1689100`
+-- Table structure for table `abonnement`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `abonnement`
---
-
+DROP TABLE IF EXISTS `abonnement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `abonnement` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `id_client` int(10) NOT NULL,
   `type_sport` varchar(255) NOT NULL,
   `total` decimal(10,0) NOT NULL,
@@ -38,46 +33,60 @@ CREATE TABLE `abonnement` (
   `date_renew` date DEFAULT NULL,
   `date_debut` date NOT NULL,
   `date_fin` date NOT NULL,
-  `status` tinyint(1) DEFAULT 1,
-  `lastPayement` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` varchar(255) NOT NULL DEFAULT 'actif',
+  `lastPayement` date NOT NULL,
+  `pause` date DEFAULT NULL,
+  `remarque` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_client` (`id_client`),
+  CONSTRAINT `abonnement_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=332 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `abonnement`
+-- Dumping data for table `abonnement`
 --
 
-INSERT INTO `abonnement` (`id`, `id_client`, `type_sport`, `total`, `payer`, `reste`, `date_abonnement`, `date_renew`, `date_debut`, `date_fin`, `status`, `lastPayement`) VALUES
-(322, 246, 'BOX', '160', '100', '60', '2022-06-06 17:17:31', NULL, '2022-06-06', '2022-07-06', 1, '2022-06-06');
-
--- --------------------------------------------------------
+LOCK TABLES `abonnement` WRITE;
+/*!40000 ALTER TABLE `abonnement` DISABLE KEYS */;
+INSERT INTO `abonnement` VALUES (331,246,'BOX',200,100,100,'2022-06-17 17:20:22',NULL,'2022-06-17','2022-06-19','actif','2022-06-17','2022-06-17','je ne suis pas content');
+/*!40000 ALTER TABLE `abonnement` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `activity`
+-- Table structure for table `activity`
 --
 
+DROP TABLE IF EXISTS `activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `activity` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `prix` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `prix` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=217 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `activity`
+-- Dumping data for table `activity`
 --
 
-INSERT INTO `activity` (`id`, `name`, `prix`) VALUES
-(214, 'BOX', '60'),
-(215, 'MUSCULATION', '100'),
-(216, 'BREAKDANCE', '200');
-
--- --------------------------------------------------------
+LOCK TABLES `activity` WRITE;
+/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
+INSERT INTO `activity` VALUES (214,'BOX',60),(215,'MUSCULATION',100),(216,'BREAKDANCE',200);
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `client`
+-- Table structure for table `client`
 --
 
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `client` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `badge` varchar(20) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
@@ -88,219 +97,110 @@ CREATE TABLE `client` (
   `adresse` text DEFAULT NULL,
   `date` date NOT NULL,
   `photo` varchar(255) DEFAULT NULL,
-  `create_by` varchar(255) NOT NULL DEFAULT 'najib'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `create_by` varchar(255) NOT NULL DEFAULT 'najib',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=247 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `client`
+-- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`id`, `badge`, `firstName`, `lastName`, `birth`, `genre`, `cin`, `tel`, `adresse`, `date`, `photo`, `create_by`) VALUES
-(246, '102030', 'flata', 'najib', '1982-11-09', 'Homme', 'ee102030', '0674176588', '106 impasse de la cerisaie', '2022-06-06', '246.jpg', 'najib flata');
-
--- --------------------------------------------------------
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (246,'102030','flata','najib','1982-11-09','Homme','ee102030','0674176588','106 impasse de la cerisaie','2022-06-06','246.jpg','najib flata');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `controlle`
+-- Table structure for table `controlle`
 --
 
+DROP TABLE IF EXISTS `controlle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `controlle` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `id_abonnement` int(10) DEFAULT NULL,
-  `id_user` varchar(255) NOT NULL DEFAULT '2'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_user` varchar(255) NOT NULL DEFAULT '2',
+  PRIMARY KEY (`id`),
+  KEY `id_abonnement` (`id_abonnement`),
+  CONSTRAINT `controlle_ibfk_1` FOREIGN KEY (`id_abonnement`) REFERENCES `abonnement` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=336 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `controlle`
+-- Dumping data for table `controlle`
 --
 
-INSERT INTO `controlle` (`id`, `date`, `id_abonnement`, `id_user`) VALUES
-(252, '2022-06-05 17:19:55', NULL, '2'),
-(253, '2022-06-05 17:19:57', NULL, '2'),
-(254, '2022-06-05 17:20:12', NULL, '2'),
-(255, '2022-06-05 17:20:30', NULL, '2'),
-(256, '2022-06-05 17:20:42', NULL, '2'),
-(257, '2022-06-05 17:21:04', NULL, '2'),
-(258, '2022-06-05 17:27:48', NULL, '2'),
-(259, '2022-06-05 17:29:20', NULL, '2'),
-(260, '2022-06-05 17:30:48', NULL, '2'),
-(261, '2022-06-05 17:34:28', NULL, '2'),
-(262, '2022-06-05 17:37:46', NULL, '2'),
-(263, '2022-06-05 17:37:55', NULL, '2'),
-(264, '2022-06-05 17:38:57', NULL, '2'),
-(265, '2022-06-05 18:05:46', NULL, '2'),
-(266, '2022-06-05 18:06:36', NULL, '2'),
-(267, '2022-06-05 18:06:41', NULL, '2'),
-(268, '2022-06-05 18:11:24', NULL, '2'),
-(269, '2022-06-05 18:13:33', NULL, '2'),
-(270, '2022-06-05 18:18:21', NULL, '2'),
-(271, '2022-06-05 18:18:25', NULL, '2'),
-(272, '2022-06-05 18:18:46', NULL, '2'),
-(273, '2022-06-05 18:18:51', NULL, '2'),
-(274, '2022-06-05 18:20:58', NULL, '2'),
-(275, '2022-06-05 18:21:03', NULL, '2'),
-(276, '2022-06-05 18:21:53', NULL, '2'),
-(277, '2022-06-05 18:22:07', NULL, '2'),
-(278, '2022-06-05 18:22:11', NULL, '2'),
-(279, '2022-06-05 18:22:16', NULL, '2'),
-(280, '2022-06-05 18:22:20', NULL, '2'),
-(281, '2022-06-05 18:24:00', NULL, '2'),
-(282, '2022-06-05 18:24:02', NULL, '2'),
-(285, '2022-06-05 18:30:23', NULL, '2'),
-(286, '2022-06-05 18:30:26', NULL, '2'),
-(331, '2022-06-06 17:22:54', 322, 'najib flata');
-
--- --------------------------------------------------------
+LOCK TABLES `controlle` WRITE;
+/*!40000 ALTER TABLE `controlle` DISABLE KEYS */;
+INSERT INTO `controlle` VALUES (252,'2022-06-05 17:19:55',NULL,'2'),(253,'2022-06-05 17:19:57',NULL,'2'),(254,'2022-06-05 17:20:12',NULL,'2'),(255,'2022-06-05 17:20:30',NULL,'2'),(256,'2022-06-05 17:20:42',NULL,'2'),(257,'2022-06-05 17:21:04',NULL,'2'),(258,'2022-06-05 17:27:48',NULL,'2'),(259,'2022-06-05 17:29:20',NULL,'2'),(260,'2022-06-05 17:30:48',NULL,'2'),(261,'2022-06-05 17:34:28',NULL,'2'),(262,'2022-06-05 17:37:46',NULL,'2'),(263,'2022-06-05 17:37:55',NULL,'2'),(264,'2022-06-05 17:38:57',NULL,'2'),(265,'2022-06-05 18:05:46',NULL,'2'),(266,'2022-06-05 18:06:36',NULL,'2'),(267,'2022-06-05 18:06:41',NULL,'2'),(268,'2022-06-05 18:11:24',NULL,'2'),(269,'2022-06-05 18:13:33',NULL,'2'),(270,'2022-06-05 18:18:21',NULL,'2'),(271,'2022-06-05 18:18:25',NULL,'2'),(272,'2022-06-05 18:18:46',NULL,'2'),(273,'2022-06-05 18:18:51',NULL,'2'),(274,'2022-06-05 18:20:58',NULL,'2'),(275,'2022-06-05 18:21:03',NULL,'2'),(276,'2022-06-05 18:21:53',NULL,'2'),(277,'2022-06-05 18:22:07',NULL,'2'),(278,'2022-06-05 18:22:11',NULL,'2'),(279,'2022-06-05 18:22:16',NULL,'2'),(280,'2022-06-05 18:22:20',NULL,'2'),(281,'2022-06-05 18:24:00',NULL,'2'),(282,'2022-06-05 18:24:02',NULL,'2'),(285,'2022-06-05 18:30:23',NULL,'2'),(286,'2022-06-05 18:30:26',NULL,'2'),(333,'2022-06-17 17:27:39',331,'najib flata'),(334,'2022-06-17 17:39:47',331,'najib flata'),(335,'2022-06-17 17:42:48',331,'najib flata');
+/*!40000 ALTER TABLE `controlle` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `roles`
+-- Table structure for table `roles`
 --
 
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
-  `id` int(10) NOT NULL,
-  `role_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `roles`
+-- Dumping data for table `roles`
 --
 
-INSERT INTO `roles` (`id`, `role_name`) VALUES
-(1, 'Admin'),
-(2, 'Editeur'),
-(3, 'Controlleur');
-
--- --------------------------------------------------------
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Admin'),(2,'Editeur'),(3,'Controlleur');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `password` varchar(20) NOT NULL,
   `date_inscription` date NOT NULL,
   `pseudo` varchar(20) NOT NULL,
-  `id_type` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_type` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_type` (`id_type`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_type`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `password`, `date_inscription`, `pseudo`, `id_type`) VALUES
-(2, 'najib flata', '123sat', '2022-05-28', 'super_admin', 1),
-(26, 'yassine flata', '10203040', '2022-05-28', 'ssine_rock10', 2),
-(27, 'chakib flata', '10203040', '2022-05-28', 'lilchak', 3);
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (2,'najib flata','123sat','2022-05-28','super_admin',1),(26,'yassine flata','10203040','2022-05-28','ssine_rock10',2),(27,'chakib flata','10203040','2022-05-28','lilchak',3);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `abonnement`
---
-ALTER TABLE `abonnement`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_client` (`id_client`);
-
---
--- Index pour la table `activity`
---
-ALTER TABLE `activity`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `controlle`
---
-ALTER TABLE `controlle`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_abonnement` (`id_abonnement`);
-
---
--- Index pour la table `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_type` (`id_type`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `abonnement`
---
-ALTER TABLE `abonnement`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=323;
-
---
--- AUTO_INCREMENT pour la table `activity`
---
-ALTER TABLE `activity`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
-
---
--- AUTO_INCREMENT pour la table `client`
---
-ALTER TABLE `client`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=247;
-
---
--- AUTO_INCREMENT pour la table `controlle`
---
-ALTER TABLE `controlle`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=332;
-
---
--- AUTO_INCREMENT pour la table `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `abonnement`
---
-ALTER TABLE `abonnement`
-  ADD CONSTRAINT `abonnement_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `controlle`
---
-ALTER TABLE `controlle`
-  ADD CONSTRAINT `controlle_ibfk_1` FOREIGN KEY (`id_abonnement`) REFERENCES `abonnement` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_type`) REFERENCES `roles` (`id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-17 17:47:52
